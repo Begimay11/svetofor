b4f60d8bbcaa7348d96cc7c3757c8051a948dbb3
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-12-23 11:18:00 +0600

    svetofor

--- svetofor/script.js
+++ svetofor/script.js
@@ -29,8 +29,6 @@
 
 
 
-
-
 const svetofor = document.querySelector('.svetofor')
 const red = document.querySelector('.red')
 const yellow = document.querySelector('.yellow')
@@ -54,6 +52,24 @@ setInterval(()=> {
         green.style.background = 'green'
     }
 },4000)
+console.log('jfh');
+// let date = new Date()
+// date()
+// console.log(date);
+// console.log(getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // ?
@@ -90,3 +106,59 @@ setInterval(()=> {
 // }
 // console.log('kfj');
 //   console.log(sameCase('','Y'));
+
+
+
+
+
+// function hello(name) {
+//     let str = arguments.length !== 0 ? name.length !== 0 ? name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() : 'World' : 'World' 
+//     return `Hello, ${str}!`
+//   }
+//   console.log(hello('kjdfnksjYHF'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sumOfDifferences(arr) {
+//     let array = []
+//     for(let i = 0; i< arr.length; i++){
+//         array.push(arr[i] - arr[i + 1])
+//     }
+//     console.log(array);
+
+//     return arr.length <= 1 ? 0 : array.filter((el)=> el).reduce((acc,el)=> acc + el)
+// }
+// console.log(sumOfDifferences([]));
+
+
+
+
+
+
+// function digitalRoot(n) {
+//     let res = n.toString().split('').reduce((acc,el)=> acc )
+//     return res
+//   }
+//   console.log(digitalRoot(1657));
+
+
+// let name = 'RidfksG'
+// console.log(name.split('').sort())
+
+function high(x){
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return x
+}
+console.log(high('man i need a taxi up to ubud'));
